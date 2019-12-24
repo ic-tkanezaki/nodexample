579fbfe2e347666af04603ed23875f7840d863da
ic-tkanezaki <t_kanezaki@insightcore.co.jp>
2019-12-24 13:10:52 +0900

    add test code

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/test', function(req, res, next) {
+  res.send("city name tokyo");
+});
+router.post('/tokyo', function(req, res, next) {
+  if(req.body.name != "tokyo") {
+    return res.statusCode(500);
+  }
   res.send("OK");
+});
+router.post('/osaka', function(req, res, next) {
+  if(req.body.name != "osaka") {
+    return res.statusCode(500);
+  }
+  res.send("OK osaka");
 });
 
 module.exports = router;
--- test/app.test.js
+++ test/app.test.js
@@ -11,7 +11,32 @@ describe('Test the root path', () => {
     test('GET /test', (done) => {
         request(app).get('/test').then((response) => {
             expect(response.statusCode).toBe(200);
+            expect(response.type).toEqual('text/html');
+            expect(response.text).toMatch(/tokyo/);
             done();
+        });
+    });
+    test('POST /tokyo', (done) => {
+        request(app)
+        .post('/tokyo')
+        .send({name: 'tokyo'}).then((response) => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+    test('POST /osaka', (done) => {
+        request(app)
+        .post('/osaka')
+        .send({name: 'osaka'})
+        .expect('Content-Type', /html/)
+        .expect(200)
+        .end((err, response) => {
+          if (err) {
+            return done(err);
+          }
+          expect(response.text).toMatch(/osaka/);
+          expect(response.text).not.toMatch(/error/);
+          return done();
         });
     });
 });
